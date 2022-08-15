fa0c00fe0110bf1f6b0b56d1dbc041ba2122dc74
koshachina <koshachina@yahoo.com>
2022-08-15 14:17:07 +0300

    sol

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,28 @@
-// export function Person({ person }) {}
+import React from 'react';
+
+export function Person({
+  person,
+}) {
+  return (
+    <>
+      <h2 className="Person__name">
+        {`My name is ${person.name}`}
+      </h2>
+      {person.age && (
+        <p className="Person__age">
+          {`I am ${person.age}`}
+        </p>
+      )}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.sex === 'f' ? `My husband's` : `My wife's`}
+          {` name is ${person.partnerName}`}
+        </p>
+      ) : (
+        <p className="Person__partner">
+          I am not married
+        </p>
+      )}
+    </>
+  );
+}
